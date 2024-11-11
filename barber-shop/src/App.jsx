27c57f86caebc3d8fafc5d1f04a8f6ec3590456c
Razawaky <viniciusduarte564@gmail.com>
2024-11-11 17:12:15 -0300

    CardWorkman alteração

--- barber-shop/src/App.jsx
+++ barber-shop/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage'
 import Calendar from './components/CalendarComponents/CalendarComponent'
+import Card from "./components/LandingPageComponents/WorkmanCard"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/calendario' element={<Calendar />} />
+          <Route path='/card=1' element={<Card />} />
         </Routes>
       </BrowserRouter>
     </div>
